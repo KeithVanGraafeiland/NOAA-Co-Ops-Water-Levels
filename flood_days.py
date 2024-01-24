1f66f6b202032497a325440853f1f52d5665dd0d
KeithVanGraafeiland <kvangraafeiland@esri.com>
2024-01-24 07:48:05 -0500

    now obtaining Station data

--- flood_days.py
+++ flood_days.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 import json
 import pandas as pd
 import re
+import arcpy
 
 
 #This is the query to get all the stations. 
@@ -11,11 +12,24 @@ waterlevels_URL = 'https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stati
 
 response = urlopen(stations_URL).read()
 # print(response)
-df= pd.read_json(stations_URL)
-# df = pd.read_json(response)
+json_object = json.loads(response)
+json_formatted_str = json.dumps(json_object, indent=2)
+print(json_formatted_str)
+df = pd.read_json(stations_URL, orient='records')
+df.drop(['count', 'units', 'self'], inplace=True, axis=1)
+df
+print(df)
+# df= pd.read_json(stations_URL, orient='records')
+# df2 = pd.json_normalize(json['response'])
+# json_schema = spark.read.json(df.rdd.map(lambda rec: rec.json_result)).schema
+# df = df.withColumn('json', F.from_json(F.col('json_result'), json_schema)) \
+#     .select("count", "units", "json.0._source.*")
+# # df = pd.read_json(response)
+# print(df2)
+# for col in df.columns:
+#     print(col)
+
+df[['x1']] = df.stations.str.split(',', expand=True)
 # print(df)
-for col in df.columns:
-    print(col)
-df_new = df['stations'].str.split(' ')
-print(df_new)
-df_new.to_csv("test1.csv")
+# df.to_csv("test1.csv")
+
